04744043983486ff7f505b3ed08963dc979e5322
An So <asong@angdasoft.com>
2025-05-12 02:24:57 -0700

    Lorem ipsum generator

--- Engineer Keyboard App/ContentView.swift
+++ Engineer Keyboard App/ContentView.swift
@@ -12,19 +12,14 @@ struct ContentView: View {
   @State private var testText: String = ""
   var body: some View {
     VStack {
-      ZStack(alignment: .leading) {
-        if testText == "" {
-          Text("Keyboard Test Area...")
-        }
-        TextEditor(text: $testText)
-          .frame(width: 300, height: 100)
-          .overlay(
-            RoundedRectangle(cornerRadius: 8)
-              .stroke(lineWidth: 1)
-          )
-      }
+      Text("Keyboard Test Area")
+      TextEditor(text: $testText)
+        .overlay(
+          RoundedRectangle(cornerRadius: 8)
+            .stroke(lineWidth: 1)
+        )
     }
-    .padding()
+    .padding(20)
   }
 }
 
--- Engineer Keyboard/KeyboardCategories/Random.swift
+++ Engineer Keyboard/KeyboardCategories/Random.swift
@@ -13,20 +13,21 @@ struct RandomCategoryView: KeyboardCategoryView {
   var body: some View {
     List {
       uuidv4View
+      loremIpsumView
     }
     .buttonStyle(.bordered)
     .listStyle(.plain)
   }
-  
+
   // MARK: UUIDv4
-  
+
   enum UUIDv4Case: String, CaseIterable {
     case upper = "UPPERCASE"
     case lower = "lowercase"
   }
-  
+
   @State var uuidv4Case: UUIDv4Case = .upper
-  
+
   func generateUUIDv4() -> String {
     var uuidString = UUID().uuidString
     if uuidv4Case == .lower {
@@ -34,7 +35,7 @@ struct RandomCategoryView: KeyboardCategoryView {
     }
     return uuidString
   }
-  
+
   var uuidv4View: some View {
     DisclosureGroup {
       VStack(alignment: .leading) {
@@ -48,12 +49,37 @@ struct RandomCategoryView: KeyboardCategoryView {
           Text("Letter case")
         }
       }
-      
+
     } label: {
       Button("UUID v4") {
         insertText(generateUUIDv4())
       }
     }
   }
-}
 
+  // MARK: Lorem ipsum
+
+  @State var loremIpsumCount: Int = 1
+
+  var loremIpsumView: some View {
+    DisclosureGroup {
+      VStack(alignment: .leading) {
+        Text("Lorem ipsum")
+        Divider()
+        Stepper(value: $loremIpsumCount, in: 1...10) {
+          Text("Paragraphs: \(loremIpsumCount)")
+        }
+      }
+    } label: {
+      Button("Lorem ipsum") {
+        insertText(generateLoremIpsum(count: loremIpsumCount))
+      }
+    }
+  }
+
+  func generateLoremIpsum(count: Int) -> String {
+    let loremIpsum =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    return (0..<count).map { _ in loremIpsum }.joined(separator: "\n\n")
+  }
+}
